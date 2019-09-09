f57eb62a7b4a089c223968d1e6bc6302927f0658
lciura <lciura.pl@gmail.com>
2019-09-09 22:30:34 +0200

    Second initial commit

--- app/containers/WilksCalculator/WilksCalculator.js
+++ app/containers/WilksCalculator/WilksCalculator.js
@@ -5,6 +5,12 @@ export default function WilksCalculator() {
   return (
     <div className="wilks-calculator">
       <h1>Wilks Calculator.</h1>
+      <div className="wilks-calculator_row">
+        <div>Waga:</div><div>76kg</div>
+      </div>
+      <div className="wilks-calculator_row">
+        <div>Podniesiony ciężar:</div><div>100kg</div>
+      </div>
     </div>
   );
 }
